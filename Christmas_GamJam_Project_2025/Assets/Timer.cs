563612cb7799e912136514cac0435e53051bf2f7
BrightestShade <raphaelwood9@gmail.com>
2025-12-15 13:31:58 +0000

    Update Timer.cs

--- Christmas_GamJam_Project_2025/Assets/Timer.cs
+++ Christmas_GamJam_Project_2025/Assets/Timer.cs
@@ -4,15 +4,23 @@ public class Timer : MonoBehaviour
 {
     [Header("Component")] 
     public TextMeshProUGUI timerText;
+    public TextMeshProUGUI highScoreText;
 
     [Header("Timer settings")]
     public float currentTime;
     public bool countDown;
 
+    [Header("highScore settings")]
+    public float fastestTime; 
+
+
     [Header("Timer Limit")]
     public bool hasLimit;
     public float timerLimit;
 
+
+    
+    
     void Start()
     {
         
@@ -30,13 +38,24 @@ public class Timer : MonoBehaviour
             timerText.color = Color.red;
             enabled = false;
             
+            
         }
         SetTimerText();
-       
+
+        if (currentTime <= fastestTime) 
+        {
+
+            PlayerPrefs.SetFloat("fastestTime", currentTime);
+            PlayerPrefs.GetFloat("fastestTime");
+           
+            
+
+        }
     }
 
     private void SetTimerText()
     {
         timerText.text = currentTime.ToString("0.0");
     }
+
 }
